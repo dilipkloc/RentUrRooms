4e429946a1a05d62b75235d68476a2edea01adc3
dilipkloc <dilipkloctech@gmail.com>
2020-02-23 12:01:34 +0530

    feat: created amenity and room through model amenity_rooms

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :amenity_rooms
   resources :amenities
   resources :roles
   resources :permissions
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_23_062648) do
+ActiveRecord::Schema.define(version: 2020_02_23_063109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2020_02_23_062648) do
   create_table "amenities", force: :cascade do |t|
     t.string "name"
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "amenity_rooms", force: :cascade do |t|
+    t.integer "room_id"
+    t.integer "amenity_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
