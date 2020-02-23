0e1b10fb2b093723ec37c82a603bbb455f7d823c
dilipkloc <dilipkloctech@gmail.com>
2020-02-23 09:25:50 +0530

    feat: devise saves extra parameters into database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  
+  def configure_permitted_parameters
+
+    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me,:first_name,:last_name,:mobile_number]
+
+    ## To permit attributes while registration i.e. sign up (app/views/devise/registrations/new.html.erb)
+    devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
+  
+    ## To permit attributes while editing a registration (app/views/devise/registrations/edit.html.erb)
+    devise_parameter_sanitizer.permit( :account_update, keys: added_attrs )
+  end
+
+
 end
