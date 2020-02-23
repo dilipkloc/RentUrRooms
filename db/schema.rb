6ba3a9f3b67a1e6d8e98e2c05afedf4306469df3
dilipkloc <dilipkloctech@gmail.com>
2020-02-23 12:04:22 +0530

    feat: cities model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_23_063109) do
+ActiveRecord::Schema.define(version: 2020_02_23_063403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 2020_02_23_063109) do
   create_table "amenity_rooms", force: :cascade do |t|
     t.integer "room_id"
     t.integer "amenity_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
