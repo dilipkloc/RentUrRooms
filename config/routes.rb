cebbec7c76830a6063c0d0e73528ac41fe0f58d9
dilipkloc <dilipkloctech@gmail.com>
2020-02-25 10:43:07 +0530

    fix: deleted permission model, cause user will have either one permission

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :amenity_rooms
   resources :amenities
   resources :roles
-  resources :permissions
   #devise_for :users
   resources :rooms
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_23_063403) do
+ActiveRecord::Schema.define(version: 2020_02_25_051115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,13 +31,6 @@ ActiveRecord::Schema.define(version: 2020_02_23_063403) do
 
   create_table "cities", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "permissions", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "role_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
