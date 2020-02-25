496c12f12c927a9d104451775d15361165d4af84
dilipkloc <dilipkloctech@gmail.com>
2020-02-25 17:44:36 +0530

    fix: user sign up role_id is mapped to Role table

--- app/controllers/my_devise/registrations_controller.rb
+++ app/controllers/my_devise/registrations_controller.rb
@@ -2,15 +2,21 @@ class MyDevise::RegistrationsController < Devise::RegistrationsController
   def create
     super
     if resource.save
-        
-      @permission = Permission.new
-      @permission.user_id = resource.id
-      @permission.role_id = Role.find_by(name: 'guest').id
-      if @permission.save
+      @user = User.find(resource.id)
+      @user.role_id = Role.find_by(name: 'guest').id
+      if @user.save
         rooms_path
       else
         errors_path
       end
+      # @permission = Permission.new
+      # @permission.user_id = resource.id
+      # @permission.role_id = Role.find_by(name: 'guest').id
+      # if @permission.save
+      #   rooms_path
+      # else
+      #   errors_path
+      # end
 
     else
       errors_path
